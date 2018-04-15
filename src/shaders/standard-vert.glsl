da86d7bcbb7dc0cb61a2f4a44fa0c8efff0e9484
Xinyu Lin <xinyulin@seas.upenn.edu>
2018-04-15 17:01:00 -0400

    one fish's movement

--- src/shaders/standard-vert.glsl
+++ src/shaders/standard-vert.glsl
@@ -30,14 +30,39 @@ void main()
     mat3 view = mat3(u_View);
     fs_Nor = vec4(view * invTranspose * vec3(vs_Nor), 0);
     vec4 worldPos = vs_Pos;
-    worldPos.x += 5.0 * sin(u_Time / 2.0);
-    mat4 rot = mat4(0.0);
-    rot[0][0] = cos(1.0 * sin(u_Time / 5.0));
-    rot[0][2] = sin(1.0 * sin(u_Time / 5.0));
-    rot[2][0] = -sin(1.0 * sin(u_Time / 5.0));
-    rot[2][2] = cos(1.0 * sin(u_Time / 5.0));
-    rot[1][1] = 1.0;
-    rot[3][3] = 1.0;
+    // worldPos.x += 5.0 * sin(u_Time / 2.0);
+
+
+    float x_time = ((worldPos.z - 8.0) + 3.0) / 40.0 * sin(worldPos.z/ 20.0 + 5.0 * u_Time / 2.0) / 2.0;
+    mat4 rotX = mat4(0.0);
+    rotX[1][1] = cos(x_time);
+    rotX[1][2] = -sin(x_time);
+    rotX[2][1] = sin(x_time);
+    rotX[2][2] = cos(x_time);
+    rotX[0][0] = 1.0;
+    rotX[3][3] = 1.0;
+    rotX = transpose(rotX);
+
+    // rotY along y axis
+    mat4 rotY = mat4(0.0);
+    rotY[0][0] = cos(1.0 * sin(u_Time / 5.0));
+    rotY[0][2] = sin(1.0 * sin(u_Time / 5.0));
+    rotY[2][0] = -sin(1.0 * sin(u_Time / 5.0));
+    rotY[2][2] = cos(1.0 * sin(u_Time / 5.0));
+    rotY[1][1] = 1.0;
+    rotY[3][3] = 1.0;
+    rotY = transpose(rotY);
+    
+    // rotZ along z axis
+    mat4 rotZ = mat4(0.0);
+    rotZ[0][0] = cos(sin(u_Time / 5.0));
+    rotZ[0][1] = -sin(sin(u_Time / 5.0));
+    rotZ[1][0] = sin(sin(u_Time / 5.0));
+    rotZ[1][1] = cos(sin(u_Time / 5.0));
+    rotZ[2][2] = 1.0;
+    rotZ[3][3] = 1.0;
+    rotZ = transpose(rotZ);
+
 
     mat4 tra = mat4(0.0);
     tra[0][0] = 1.0;
@@ -47,12 +72,11 @@ void main()
     tra[0][3] = 5.0;
     tra[1][3] = 10.0;
     tra[2][3] = 5.0;
-
     tra = transpose(tra);
-    rot = transpose(rot);
+    
     
 
-    // worldPos =   tra * rot * worldPos; 
+    worldPos =     rotX * worldPos; 
 
     fs_Pos = u_View * u_Model * worldPos;
     
